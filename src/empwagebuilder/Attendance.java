1e2077a23885dc202a42ddf027b54f20b1b3b719
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-04-26 17:02:47 +0530

    Added: welcome msg

---
+++ src/empwagebuilder/Attendance.java
@@ -0,0 +1,16 @@
+package empwagebuilder;
+
+public class Attendance {
+
+	public static void main(String[] args) {
+		int is_full_time=1;
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if (empcheck==is_full_time)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+		// TODO Auto-generated method stub
+
+	}
+
+}
