dfb4e21ea0b29bb45f703148124b542e5302e32c
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-05 20:24:59 +0530

    Added: Attendance of employees checked

--- src/empwagebuilder/EmployeeWageBuilder.java
+++ src/empwagebuilder/EmployeeWageBuilder.java
@@ -1,15 +1,15 @@
 package empwagebuilder;
 
-public class Attendance {
+public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
-		int is_full_time=1;
-		double empcheck=Math.floor(Math.random()*10)%2;
-		if (empcheck==is_full_time)
+
+		int is_full_time = 1;
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == is_full_time)
 			System.out.println("Employee is present");
 		else
 			System.out.println("Employee is absent");
-		// TODO Auto-generated method stub
 
 	}
 
