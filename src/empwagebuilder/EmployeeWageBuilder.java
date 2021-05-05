a9439f347ba0b90847e4609c3cb9e7ec2a1ad3f5
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-06 00:28:12 +0530

    Added: Switch case statement added

--- src/empwagebuilder/EmployeeWageBuilder.java
+++ src/empwagebuilder/EmployeeWageBuilder.java
@@ -4,8 +4,8 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-	private static final int EMP_FULLTIMEHOUR=8;
-	private static final int EMP_PARTTIMEHR=4;
+	private static final int EMP_FULLTIMEHOUR = 8;
+	private static final int EMP_PARTTIMEHR = 4;
 
 	public static void main(String[] args) {
 
@@ -13,14 +13,31 @@ public class EmployeeWageBuilder {
 		double empcheck = Math.floor(Math.random() * 10) % 2;
 		if (empcheck == is_full_time) {
 			System.out.println("Employee is present");
-		    System.out.println("Daily employee wage is: "+EMP_RATE_PER_HOUR*EMP_FULLTIMEHOUR);
-			
-		}
-			
-		else
-			System.out.println("Half day wage of emp is:"+EMP_RATE_PER_HOUR*EMP_PARTTIMEHR);
-		
+			System.out.println("Daily employee wage is: " + EMP_RATE_PER_HOUR * EMP_FULLTIMEHOUR);
 
+		}
+		else {
+			
+				System.out.println("Half day wage of emp is:" + EMP_RATE_PER_HOUR * EMP_PARTTIMEHR);
+
+		    
+		}
+		int myCheck=(int) empcheck;
+		switch(myCheck) {
+		case 0:
+			System.out.println("Employee is Absent !!");
+			break;
+		case 1:
+			System.out.println("Employee is Present !!");
+			break;
+		}
+
+		
+	    
+	    
 	}
+		
+	
+	   
 
 }
