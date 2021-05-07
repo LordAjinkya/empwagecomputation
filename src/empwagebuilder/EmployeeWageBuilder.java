ff8468380007e2ead5a6e968c3b4417671b91412
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-08 00:36:30 +0530

    Added:code to save empwage for each company

--- src/empwagebuilder/EmployeeWageBuilder.java
+++ src/empwagebuilder/EmployeeWageBuilder.java
@@ -3,6 +3,20 @@ package empwagebuilder;
 public class EmployeeWageBuilder {
 	public final static int IS_FULL_TIME = 1;
 	public final static int IS_PART_TIME = 0;
+	private final String company;
+	private final int empRatePerHr;
+	private final int workingDayPerMonth;
+	private final int maxHrInMonth;
+	private int totalEmpWage;
+
+	public EmployeeWageBuilder(String company, int empRatePerHr, int workingDayPerMonth, int maxHrInMonth) {
+		super();
+		this.company = company;
+		this.empRatePerHr = empRatePerHr;
+		this.workingDayPerMonth = workingDayPerMonth;
+		this.maxHrInMonth = maxHrInMonth;
+	}
+	
 	public static void empWageCalculation(String company, int empRatePerHr, int workingDayPerMonth, int maxHrInMonth) {
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDay = 0;
 		// Using switch Case
@@ -23,18 +37,23 @@ public class EmployeeWageBuilder {
 			totalEmpHrs += empHrs;
 			System.out.println("Total Working Days: " + totalWorkingDay + " " + "Total EMP Hours:" + totalEmpHrs);
 		}
-		System.out.println("Total Emp Wage For Company:-->" + company + " is:" + totalEmpHrs * empRatePerHr);
+		totalEmpWage = totalEmpHrs * empRatePerHr;
+	}
+	public String toString() {
+		return "[Total Emp Wage for: "+company+" is: " + totalEmpWage + "]";
 	}
 	
 
 
 	public static void main(String[] args) 
 	{
-		empWageCalculation("D-MART", 20, 2, 10);
-		empWageCalculation("JIO", 20, 2, 10);
-		System.out.println("Welcome to employee wage computation");
-		Wagecomputation wc = new Wagecomputation();
-		wc.computation();
+		System.out.println("Welcome to Employee Wage Computation !!");
+		EmployeeWageBuilder dmart = new EmployeeWageBuilder("D-MART", 20, 2, 10);
+		EmployeeWageBuilder jio = new EmployeeWageBuilder("JIO", 20, 2, 10);
+		dmart.empWageCalculation();
+		System.out.println(dmart);
+		jio.empWageCalculation();
+		System.out.println(jio);
 	}
 
 }
